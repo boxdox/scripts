445cdaec56f360a4a919cb2f75c47fab2882a7a2
vaibhavkandwal <vaibhavkandwal1210@gmail.com>
2016-06-25 10:03:45 +0530

    god-mode-creator added

---
+++ god-mode-creator-for-win7-8-10.bat
@@ -0,0 +1,19 @@
+mkdir "GodMode.{ED7BA470-8E54-465E-825C-99712043E01C}
+mkdir "Special Folders
+cd ".\Special Folders
+mkdir "God Mode.{ED7BA470-8E54-465E-825C-99712043E01C}
+mkdir "Location Settings.{00C6D95F-329C-409a-81D7-C46C66EA7F33}
+mkdir "Biometric Settings.{0142e4d0-fb7a-11dc-ba4a-000ffe7ab428}
+mkdir "Power Settings.{025A5937-A6BE-4686-A844-36FE4BEC8B6D}
+mkdir "Icons And Notifications.{05d7b0f4-2121-4eff-bf6b-ed3f69b894d9}
+mkdir "Credentials and Logins.{1206F5F1-0569-412C-8FEC-3204630DFB70}
+mkdir "Programs and Features.{15eae92e-f17a-4431-9f28-805e482dafd4}
+mkdir "Default Programs.{17cd9488-1228-4b2f-88ce-4298e93e0966}
+mkdir "All NET Frameworks and COM Libraries.{1D2680C9-0E2A-469d-B787-065558BC7D43}
+mkdir "All Networks For Current Connection.{1FA9085F-25A2-489B-85D4-86326EEDCD87}
+mkdir "Network.{208D2C60-3AEA-1069-A2D7-08002B30309D}
+mkdir "My Computer.{20D04FE0-3AEA-1069-A2D8-08002B30309D}
+mkdir "Printers.{2227A280-3AEA-1069-A2DE-08002B30309D}
+mkdir "Application Connections.{241D7C96-F8BF-4F85-B01F-E2B043341A4B}
+mkdir "Firewall and Security.{4026492F-2F69-46B8-B9BF-5654FC07E423}
+mkdir "Performance.{78F3955E-3B90-4184-BD14-5397C15F1EFC}
