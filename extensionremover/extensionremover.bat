03b4d81379a4a246f40fca897c95e081506026f1
Vaibhav Kandwal <vaibhavkandwal@users.noreply.github.com>
2017-06-02 23:56:16 +0530

    Update extensionremover.bat
    added recursive rename ability

--- extensionremover/extensionremover.bat
+++ extensionremover/extensionremover.bat
@@ -7,7 +7,20 @@ echo.
 echo.
 echo.
 set /p extension="Enter the extension to remove (without the .): "
+set /p choice="Do you want to remove extension of files in sub directories too? [Y/N]: "
+IF /i "%choicethis%"=="Y" IF /i "%choicethis%"=="y" goto Recursive
+IF /i "%choicethis%"=="N" IF /i "%choicethis%"=="n" goto Normal
+
 echo Trying to remove all the extra extension!
+
+:Normal
 RENAME *.%extension% *.
+goto commonexit
+
+:Recursive
+for /r %%x in (*.%extension%) do ren "%%x" *.
+goto commonexit
+
+:commonexit
 echo Done!
 pause
